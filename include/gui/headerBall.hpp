f06676672717a3f8cf4fa5335e6005907ffd08fa
SJD23Dev <SJD23Dev@proton.me>
2025-01-14 14:36:22 -0600

    Rename orbitAngle to headerAngle in HeaderBall class and update related methods for clarity

--- include/gui/headerBall.hpp
+++ include/gui/headerBall.hpp
@@ -3,14 +3,14 @@
 #include <cmath>
 #include <SFML/Graphics.hpp>
 
-struct HeaderBall : public sf::Drawable{
+struct HeaderBall : public sf::Drawable {
     sf::Text text;
     sf::Font font;
     sf::Vector2f currentPosition;
-    float currentOrbitAngle;
+    float currentHeaderAngle;
 
     HeaderBall();
-    void update(const sf::Vector2f& playerPosition, float orbitAngle);
+    void update(const sf::Vector2f& playerPosition, float angle);
 
 private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
--- src/gui/headerBall.cpp
+++ src/gui/headerBall.cpp
@@ -2,7 +2,7 @@
 
 HeaderBall::HeaderBall() {
     currentPosition = sf::Vector2f(0.0f, 0.0f);
-    currentOrbitAngle = 0.0f;
+    currentHeaderAngle = 0.0f;
     font.loadFromFile("../assets/fonts/Cocogoose-Pro-Light-trial.ttf");
     text.setFont(font);
     text.setCharacterSize(12);
@@ -11,19 +11,19 @@ HeaderBall::HeaderBall() {
     text.setOrigin(text.getLocalBounds().width / 2, text.getLocalBounds().height / 2);
 }
 
-void HeaderBall::update(const sf::Vector2f& playerPosition, float targetOrbitAngle) {
+void HeaderBall::update(const sf::Vector2f& playerPosition, float angle) {
     const float radius = 30.0f;
 
     float smoothingFactor = 0.35f; // 0.0f = no smoothing
-    currentOrbitAngle += (targetOrbitAngle - currentOrbitAngle) * smoothingFactor;
+    currentHeaderAngle += (angle - currentHeaderAngle) * smoothingFactor;
 
     currentPosition = sf::Vector2f(
-        playerPosition.x + radius * std::cos(currentOrbitAngle),
-        playerPosition.y + radius * std::sin(currentOrbitAngle)
+        playerPosition.x + radius * std::cos(currentHeaderAngle),
+        playerPosition.y + radius * std::sin(currentHeaderAngle)
     );
 
     text.setPosition(currentPosition);
-    text.setRotation(currentOrbitAngle * 180 / M_PI);
+    text.setRotation(currentHeaderAngle * 180 / M_PI);
 }
 
 void HeaderBall::draw(sf::RenderTarget& target, sf::RenderStates states) const {

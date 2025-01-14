43241b712465cf158a2ff1f9fc4035035f01b5fa
SJD23Dev <SJD23Dev@proton.me>
2025-01-14 14:31:38 -0600

    Refactor PlayerCharacter class: rename shape to body, streamline thrust bar updates, and enhance position handling

--- src/entities/playerCharacter.cpp
+++ src/entities/playerCharacter.cpp
@@ -1,33 +1,28 @@
 #include "entities/playerCharacter.hpp"
 
-PlayerCharacter::PlayerCharacter() : headerAngle(0.0f), velocity(0.0f, 0.0f), thrust(0) {
-    shape.setRadius(10.f);
-    shape.setFillColor(sf::Color(25, 191, 225));
-    shape.setOrigin(shape.getRadius(), shape.getRadius());
-    position = sf::Vector2f(500.0f, 500.0f);
-    shape.setPosition(position);
+PlayerCharacter::PlayerCharacter()
+    : headerAngle(0.0f)
+    , velocity(0.0f, 0.0f)
+    , thrust(0) {
 
-    font.loadFromFile("../assets/fonts/Cocogoose-Pro-Darkmode-trial.ttf");
-    for (int i = 0; i < 5; i++) {
-        thrustBars[i].setFont(font);
-        thrustBars[i].setCharacterSize(8);
-        thrustBars[i].setFillColor(sf::Color(255, 255, 255, 75));
-        thrustBars[i].setString("|");
-        thrustBars[i].setOrigin(thrustBars[i].getLocalBounds().width / 2, thrustBars[i].getLocalBounds().height / 2);
-    }
-    updateThrustBars();
+    body.setRadius(10.f);
+    body.setFillColor(sf::Color(25, 191, 225));
+    body.setOrigin(body.getRadius(), body.getRadius());
+
+    position = sf::Vector2f(500.0f, 500.0f);
+    body.setPosition(position);
+
+    thrustBars.update(*this);
 }
 
 void PlayerCharacter::update() {
     move();
-    updateThrustBars();
+    thrustBars.update(*this);
 }
 
 void PlayerCharacter::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    target.draw(shape, states);
-    for (const auto& bar : thrustBars) {
-        target.draw(bar, states);
-    }
+    target.draw(body, states);
+    target.draw(thrustBars, states);
     target.draw(headerBall, states);
 }
 
@@ -44,41 +39,26 @@ void PlayerCharacter::move() {
             thrustMultiplier = 1.5f; // "Run" Speed
             break;
         case 4:
-            thrustMultiplier = 3.0f; // TODO: Decide what to do with this
+            thrustMultiplier = 3.0f; 
             break;
         case 5:
-            thrustMultiplier = 3.5f; // TODO: Decide what to do with this
+            thrustMultiplier = 3.5f; 
             break;
         default:
             break;
     }
 
+    /*
+    // Velosity is a vector of (x, y), where 'x' is the cos of the header angle,
+    // and 'y' is the sin of the header angle. Velocity is then multiplied by the
+    // current thrust mulitplier and a constant value of 0.1f.
+    */
+
     velocity += sf::Vector2f(std::cos(headerAngle), std::sin(headerAngle)) * thrustMultiplier * 0.1f;
-    velocity *= 0.99f;
+    velocity *= 0.99f; // Create a drag effect (lower = more drag)
 
     position += velocity;
-    shape.setPosition(position);
 
-    headerBall.update(position, headerAngle);
-}
-
-void PlayerCharacter::updateThrustBars() {
-    float barSpacing = 4.0f; // Spacing between bars
-    for (int i = 0; i < 5; ++i) {
-        thrustBars[i].setPosition(position.x - (2 * barSpacing) + (i * barSpacing), position.y + shape.getRadius() + 6);
-        if (i < thrust) {
-            if (i == 0) {
-                thrustBars[i].setFillColor(sf::Color(60, 136, 159));
-            } else if (i < 3) {
-                thrustBars[i].setFillColor(sf::Color(144, 233, 75, 180)); // Green for first three bars
-            } else if (i == 3) {
-                thrustBars[i].setFillColor(sf::Color(219, 167, 45, 180)); // Orange for the fourth bar
-            } else if (i == 4) {
-                thrustBars[i].setFillColor(sf::Color(222, 75, 75, 180)); // Red for the fifth bar
-            }
-            
-        } else {
-            thrustBars[i].setFillColor(sf::Color(255, 255, 255, 75)); // Semi-transparent for inactive thrust
-        }
-    }
+    body.setPosition(position); // Update player position
+    headerBall.update(position, headerAngle); // Update headerBall position
 }
